f4ff705e3d0e2ef730a08823aa81bbbc7a1e484f
Gints Polis <gints.polis@lattelecom.lv>
2019-04-20 11:21:33 +0300

    Fix #3 Auth server registers itself for consul service.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
 import server from './server'
+const consul = require('consul')()
+const port = 4000
 
-server.start({ port: 4000 }, () =>
+require('dotenv').config()
+
+server.start({ port }, () => {
   console.log(`Server is running on http://localhost:4000`)
-)
+  const CONSUL_ID = require('uuid').v4()
+  const details = {
+    name: 'auth',
+    address: process.env.CONSUL_HOST || 'localhost',
+    port,
+    id: CONSUL_ID,
+    check: {
+      ttl: '10s',
+      deregister_critical_service_after: '1m'
+    }
+  }
+  consul.agent.service.register(details, err => {
+    setInterval(() => {
+      consul.agent.check.pass({ id: `service:${CONSUL_ID}` }, err => {
+        if (err) throw new Error(err)
+      })
+    }, 5 * 1000)
+    process.on('SIGINT', () => {
+      console.log('SIGINT. De-Registering...')
+      const details = { id: CONSUL_ID }
+
+      consul.agent.service.deregister(details, err => {
+        console.log('de-registered.', err)
+        process.exit()
+      })
+    })
+  })
+})
